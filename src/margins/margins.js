013f0db96d89a3ecfe2544fe27fd78ccb532c571
Rafał Kucharski <qhr666@gmail.com>
2017-11-10 01:00:02 +0100

    Added responsivenes

--- src/margins/margins.js
+++ src/margins/margins.js
@@ -4,10 +4,15 @@ const margin = { top: 10, right: 10, bottom: 20, left: 30 };
 const width = 425 - margin.left - margin.right;
 const height = 600 - margin.top - margin.bottom;
 
+const fullWidth = width + margin.left + margin.right;
+const fullHeight = height + margin.top + margin.bottom;
+
 const svg = d3.select('.chart')
   .append('svg')
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom)
+  .call(responsivefy)
+  // .attr('viewBox', `0 0 ${fullWidth} ${fullHeight}`) // view box allow you to control which part of svg will be visible
   .append('g')
   .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
@@ -23,7 +28,7 @@ const yScale = d3.scaleLinear()
   .range([height, 0]);
 
 // axis automaticly creates proper axis from bottom to top
-const yAxis = d3.axisLeft(yScale)
+const yAxis = d3.axisLeft(yScale).style('fill', 'cyan')
   // .ticks(8); //ticks do not guarantee number of ticks presented !!!!
   .tickValues([20, 40, 60, 80])
   .tickSize(0)
@@ -42,3 +47,29 @@ svg.append('g')
   .call(xAxis);
 
 
+function responsivefy(svg) {
+  // get container + svg aspect ratio
+  var container = d3.select(svg.node().parentNode),
+    width = parseInt(svg.style("width")),
+    height = parseInt(svg.style("height")),
+    aspect = width / height;
+
+  // add viewBox and preserveAspectRatio properties,
+  // and call resize so that svg resizes on inital page load
+  svg.attr("viewBox", "0 0 " + width + " " + height)
+    .attr("preserveAspectRatio", "xMinYMid")
+    .call(resize);
+
+  // to register multiple listeners for same event type,
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you call invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on("resize." + container.attr("id"), resize);
+
+  // get width of container and resize svg to fit it
+  function resize() {
+    var targetWidth = parseInt(container.style("width"));
+    svg.attr("width", targetWidth);
+    svg.attr("height", Math.round(targetWidth / aspect));
+  }
+}
